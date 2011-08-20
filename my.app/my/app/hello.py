5a37504b9eb0ceb6a039f6afe8593d1f8a49f626
Alex Clark <aclark@aclark.net>
2011-08-20 19:08:58 -0400

    Hello world!

--- my.app/my/app/hello.py
+++ my.app/my/app/hello.py
@@ -1,3 +1,10 @@
-from zope.publisher.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.publisher.browser import BrowserPage
 
-class HelloWorld(BrowserView):
+
+class Hello(BrowserPage):
+
+    template = ViewPageTemplateFile('hello.pt')
+
+    def __call__(self):
+        return self.template()
